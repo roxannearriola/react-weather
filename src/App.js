4ceb01920f8390d5ebf195da4a7f021ab034d822
roxannearriola <roxanne.arriola@gmail.com>
2021-07-13 16:22:51 -0400

    Added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+			<footer>
+				This project was coded by Roxanne A. and is{" "}
+				<a
+					href="https://github.com/roxannearriola/react-weather"
+					target="_blank"
+				>
+					open-sourced on GitHub
+				</a>{" "}
+				and{" "}
+				<a href="https://eager-joliot-601a25.netlify.app/" target="_blank">
+					hosted on Netlify
+				</a>
+				.
+			</footer>
+		</div>
+	);
 }
