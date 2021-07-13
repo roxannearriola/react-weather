999725d8d17ee61b35bd1018abcddd0b5a4efb78
roxannearriola <roxanne.arriola@gmail.com>
2021-07-13 16:54:35 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Weather App</h1>
-			<footer>
-				This project was coded by Roxanne A. and is{" "}
-				<a
-					href="https://github.com/roxannearriola/react-weather"
-					target="_blank"
-					rel="noreferrer"
-				>
-					open-sourced on GitHub
-				</a>{" "}
-				and{" "}
-				<a
-					href="https://eager-joliot-601a25.netlify.app/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					hosted on Netlify
-				</a>
-				.
-			</footer>
+			<div className="container">
+				<h1>Weather App</h1>
+				<footer>
+					This project was coded by Roxanne A. and is{" "}
+					<a
+						href="https://github.com/roxannearriola/react-weather"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open-sourced on GitHub
+					</a>{" "}
+					and{" "}
+					<a
+						href="https://eager-joliot-601a25.netlify.app/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						hosted on Netlify
+					</a>
+					.
+				</footer>
+			</div>
 		</div>
 	);
 }
